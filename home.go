82ab01705c556dc98603f206cea5e6b6e6e47290
Derek Reid <itsjustdel@gmail.com>
2022-05-24 12:42:55 +0100

    Reverted to initial design

--- home.go
+++ home.go
@@ -24,16 +24,6 @@ func (n *nomad) makeHome() fyne.CanvasObject {
 	zone, _ := time.LoadLocation("Europe/London")
 	cell := newLocation("Edinburgh", "United Kingdom", zone)
 
-	c := container.New(&nomadLayout{},
+	return container.New(&nomadLayout{},
 		cell, n.makeAddCell())
-
-	c.Hide()
-
-	return c
-}
-
-func (n *nomad) showHome(obj fyne.CanvasObject, splashFadeTime int) {
-	time.Sleep(time.Second * time.Duration(splashFadeTime))
-	obj.Show()
-	n.main.Content().Refresh()
 }
