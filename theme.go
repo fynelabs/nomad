7a02bafb183f7df418da1494c4910e270fec4ed8
Derek Reid <itsjustdel@gmail.com>
2022-05-24 10:23:35 +0100

    Create theme.go

---
+++ theme.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+)
+
+type myTheme struct{}
+
+func (m myTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameBackground {
+		return color.RGBA{24, 12, 39, 1}
+	}
+	return theme.DefaultTheme().Color(name, variant)
+}
+
+func (m myTheme) Font(style fyne.TextStyle) fyne.Resource {
+	return resourceOpenSansExtraBoldItalicTtf
+}
+
+func (m myTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(name)
+}
+
+func (m myTheme) Size(name fyne.ThemeSizeName) float32 {
+	return theme.DefaultTheme().Size(name)
+}
