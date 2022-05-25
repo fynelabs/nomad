c6b89d4bee3427fb6c4dff3e8c35be882e8f7d1c
Andy Williams <andy@andy.xyz>
2022-05-25 13:49:46 +0100

    Save data to preferences and load again

--- data.go
+++ data.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"time"
 
 	"fyne.io/fyne/v2"
@@ -27,8 +28,25 @@ func newCityStore(p fyne.Preferences) *cityStore {
 	s := &cityStore{prefs: p}
 
 	zone, _ := time.LoadLocation("Europe/London")
-	s.list = []*city{
-		newCity("Edinburgh", "United Kingdom", zone),
+	count := p.Int(preferenceKeyPrefix + "count")
+	if count == 0 {
+		s.list = []*city{
+			newCity("Edinburgh", "United Kingdom", zone),
+		}
+		s.save()
+	} else {
+		s.list = make([]*city, count)
+		for i := 0; i < count; i++ {
+			prefix := preferenceKeyPrefix + strconv.Itoa(i) + "."
+			name := p.StringWithFallback(prefix+"name", "No City")
+			country := p.StringWithFallback(prefix+"country", "No Country")
+			zoneName := p.StringWithFallback(prefix+"zone", "UTC")
+			zone, err := time.LoadLocation(zoneName)
+			if err != nil {
+				fyne.LogError("Failed to load timezone "+zoneName, err)
+			}
+			s.list[i] = newCity(name, country, zone)
+		}
 	}
 
 	return s
@@ -36,4 +54,15 @@ func newCityStore(p fyne.Preferences) *cityStore {
 
 func (s *cityStore) cities() []*city {
 	return s.list
+}
+
+func (s *cityStore) save() {
+	s.prefs.SetInt(preferenceKeyPrefix+"count", len(s.list))
+
+	for i, c := range s.list {
+		prefix := preferenceKeyPrefix + strconv.Itoa(i) + "."
+		s.prefs.SetString(prefix+"name", c.name)
+		s.prefs.SetString(prefix+"country", c.country)
+		s.prefs.SetString(prefix+"zone", c.localTime.Location().String())
+	}
 }
