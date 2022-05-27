b08919e8e6a0ac79752f9345df16303d90023e47
Andy Williams <andy@andy.xyz>
2022-05-27 21:31:07 +0100

    Fix layout to have space between cells and on desktop a border around the whole app

--- layout.go
+++ layout.go
@@ -5,31 +5,40 @@ import "fyne.io/fyne/v2"
 const (
 	minWidth  float32 = 300
 	minHeight float32 = 200
+	cellSpace float32 = 16
 )
 
 type nomadLayout struct{}
 
 func (l *nomadLayout) Layout(objs []fyne.CanvasObject, s fyne.Size) {
-	cols := int(s.Width / minWidth)
-	colWidth := s.Width / float32(cols)
+	outer := cellSpace
+	if fyne.CurrentDevice().IsMobile() {
+		outer = 0
+	}
+	cols := int((s.Width - outer) / (minWidth + outer))
+	colWidth := (s.Width-outer)/float32(cols) - outer
 	cellSize := fyne.NewSize(colWidth, minHeight)
 
 	offset := 0
-	pos := fyne.Position{}
+	pos := fyne.Position{X: outer, Y: outer}
 	for _, o := range objs {
 		o.Resize(cellSize)
 		o.Move(pos)
 
 		offset++
-		pos.X += colWidth
+		pos.X += colWidth + cellSpace
 		if offset >= cols {
 			offset = 0
-			pos.X = 0
-			pos.Y += minHeight
+			pos.X = outer
+			pos.Y += minHeight + cellSpace
 		}
 	}
 }
 
 func (l *nomadLayout) MinSize(_ []fyne.CanvasObject) fyne.Size {
-	return fyne.NewSize(minWidth, minHeight*2)
+	if fyne.CurrentDevice().IsMobile() {
+		return fyne.NewSize(minWidth, minHeight*2+cellSpace)
+	}
+
+	return fyne.NewSize(minWidth+cellSpace*2, minHeight*2+cellSpace*3)
 }
