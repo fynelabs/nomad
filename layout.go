b363c67b1e8c170febb0c7eaf570e657678839ed
Derek Reid <itsjustdel@gmail.com>
2022-07-19 15:11:45 +0100

    Create extra space to scroll

--- layout.go
+++ layout.go
@@ -48,7 +48,7 @@ func (l *nomadLayout) MinSize(cells []fyne.CanvasObject) fyne.Size {
 		cols = 1
 	}
 	rows := int(math.Ceil(float64(len(cells)) / float64(l.cols)))
-	height := float32((minHeight+cellSpace)*float32(rows) - cellSpace)
+	height := float32((minHeight+cellSpace*4)*float32(rows) - cellSpace)
 	if fyne.CurrentDevice().IsMobile() {
 		return fyne.NewSize(minWidth, height)
 	}
