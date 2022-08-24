fb0ff62489ee1087d90bae9aa4509c3d9f552ceb
Andy Williams <andy@andy.xyz>
2022-08-24 22:41:00 +0100

    Add a simple extension to show systray on desktop platforms

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"time"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
 )
 
 type nomad struct {
@@ -29,6 +32,44 @@ func main() {
 	w.Resize(fyne.NewSize(300, 500))
 	w.SetIcon(resourceIconPng)
 
+	if deskApp, ok := a.(desktop.App); ok {
+		w.SetCloseIntercept(func() {
+			w.Hide() // don't close the window if system tray used
+		})
+
+		setupSystray(deskApp, w, store)
+	}
 	go fadeSplash(splash)
 	w.ShowAndRun()
+}
+
+func setupSystray(a desktop.App, w fyne.Window, store *cityStore) {
+	a.SetSystemTrayIcon(resourceIconPng)
+	setupSystrayMenu(a, w, store)
+
+	go func() {
+		for range time.NewTicker(time.Minute).C {
+			setupSystrayMenu(a, w, store)
+		}
+	}()
+}
+
+func setupSystrayMenu(a desktop.App, w fyne.Window, store *cityStore) {
+	times := make([]*fyne.MenuItem, len(store.list)+2)
+	times[0] = fyne.NewMenuItem("Show Nomad", func() {
+		w.Show()
+	})
+	times[1] = fyne.NewMenuItemSeparator()
+
+	for i, item := range store.list {
+		locDate := time.Now().In(item.localTime.Location())
+		local := locDate.Format("15:04")
+		label := item.name + ": " + local
+
+		localTime := fyne.NewMenuItem(label, nil)
+		localTime.Disabled = true
+		times[i+2] = localTime
+	}
+
+	a.SetSystemTrayMenu(fyne.NewMenu("Times", times...))
 }
